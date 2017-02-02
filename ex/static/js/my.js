14c3deb4bd58b460bc89332e67ab3c44924794e5
DennyD <Denis.Avramenko1705@gmail.com>
2017-02-02 21:24:13 +0300

    some styles, vk-link

--- ex/static/js/my.js
+++ ex/static/js/my.js
@@ -7,18 +7,6 @@ $(document).ready(function(){
     });
 });
 
-/*
-$(document).ready(function ()
-    $('.btn').click(function(){
-        var catid;
-        catid = $(this).attr("data-catid");
-        var uniq_id = ".ajax_"+catid;
-        $.get('/like_post/', {post_id: catid}, function(data){
-            $(uniq_id).html(data);
-        });
-    });
-});
-*/
 
 function getAjax(divdett,pageload) {
     $(function(){$(divdett).load(pageload).show()
@@ -38,9 +26,3 @@ $(document).ready(function () {
     });
 })
 
-
-/**
-$(document).ready(function () {
-    $('.content').height($(document).height() - $('footer').height() - $('nav').height());
-})
-**/
